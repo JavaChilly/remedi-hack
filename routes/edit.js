ad92fff5000ce5bb542fdea937f3b40fc535ec3d
Chad Hill <chilly@gmail.com>
2012-07-28 14:27:12 -0700

    added editor route

---
+++ routes/edit.js
@@ -0,0 +1,15 @@
+var exports = module.exports;
+
+var doctorEditor = function(id) {
+	
+}
+
+exports.showEditor = function( req, res, next ) {
+	var id = req.params.id;
+	var placetype = req.params.placetype;
+	
+}
+
+exports.saveEditor = function( req, res, next ) {
+	
+}
